a90c92051beaacf94c142654b7435a987c9f75eb
mohamed alaa <m-aa2001@hotmail.com>
2024-11-25 18:26:32 +0200

    2nd task

--- Source.cpp
+++ Source.cpp
@@ -1,13 +1,14 @@
-#include <iostream>
+﻿#include <iostream>
 #include <gl/glew/glew.h>
 #include <SFML/Graphics.hpp>
 #include <SFML/OpenGL.hpp>
 #include<gl\glm\glm.hpp>
 
-const float PI = 22/7;
+
+const float PI = 3.14159f;
 using namespace std;
 using namespace glm;
-
+int SierpinskiDepth = 5;
 enum DrawingMode
 {
 	Points,
@@ -18,34 +19,52 @@ enum DrawingMode
 struct Vertex
 {
 	vec3 Position;
-	vec3 COlor;
+	vec3 Color;
 };
 
 GLuint InitShader(const char* vertex_shader_file_name, const char* fragment_shader_file_name);
 
 const GLint WIDTH = 600, HEIGHT = 600;
-GLuint VBO, BasiceprogramId;
+GLuint VBO, BasicProgramId;
 DrawingMode Current_DrawingMode = DrawingMode::Lines;
 
-void CreateColoredCircle(GLfloat r)
-{
-	Vertex CircleVertices[50];
-	r /= WIDTH;
-	float delta = 0;
-	for (int i = 0; i < 50; i++)
-	{
-		if (i%2 == 0)
-			CircleVertices[i] = { vec3(r * cos(delta),r * sin(delta) , 0), vec3(1,1,0) };
-		else
-		CircleVertices[i] = { vec3(r*cos(delta),r * sin(delta) , 0), vec3(0.5,0.4,0)};
-		delta += (2* PI / 49);
+void GenerateSierpinski(std::vector<Vertex>* vertices, vec3 a, vec3 b, vec3 c, int depth) {
+	if (depth == 0) {
+		vec3 color = vec3((a.x + b.x + c.x) / 3.0f, (a.y + b.y + c.y) / 3.0f, 1.0f);
+		vertices->push_back({ a, color });
+		vertices->push_back({ b, color });
+		vertices->push_back({ c, color });
+		return;
 	}
+	else {
+		vec3 ab = (a + b) * 0.5f;
+		vec3 bc = (b + c) * 0.5f;
+		vec3 ca = (c + a) * 0.5f;
+
+		GenerateSierpinski(vertices, a, ab, ca, depth - 1);
+		GenerateSierpinski(vertices, ab, b, bc, depth - 1);
+		GenerateSierpinski(vertices, ca, bc, c, depth - 1);
+	}
+}
+void CreateColoredSierpinski(float depth)
+{
+	vector<Vertex> Sierpinski;
+
+	vec3 v1(-1, -1, 0); 
+	vec3 v2(1, -1, 0);   
+	vec3 v3(0, 1, 0);  
+
+	vector<Vertex> vertices;
+
+	GenerateSierpinski(&vertices, v1, v2, v3, depth);
+
 	// create buffer object
 	glGenBuffers(1, &VBO);
 
 	// binding buffer object
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(CircleVertices), CircleVertices, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), vertices.data(), GL_STATIC_DRAW);
+
 
 	// shader
 	glVertexAttribPointer(0, 3, GL_FLOAT, false, sizeof(Vertex), 0);
@@ -54,6 +73,9 @@ void CreateColoredCircle(GLfloat r)
 	glVertexAttribPointer(1, 3, GL_FLOAT, false, sizeof(Vertex), (char*)(3 * sizeof(GL_FLOAT)));
 	glEnableVertexAttribArray(1);
 }
+
+
+
 
 void CompileShader(const char* vertex_shader_file_name, const char* fragment_shader_file_namering, GLuint& programId)
 {
@@ -81,9 +103,10 @@ int Init()
 	cout << "\tVersion: " << glGetString(GL_VERSION) << endl;
 	cout << "\tGLSL:" << glGetString(GL_SHADING_LANGUAGE_VERSION) << endl;
 
-	CompileShader("VS.glsl", "FS.glsl", BasiceprogramId);
-	CreateColoredCircle(300);
+	CompileShader("VS.glsl", "FS.glsl", BasicProgramId);
+	//CreateColoredCircle(300);
 	//CreateColoredTriangle();
+	CreateColoredSierpinski(SierpinskiDepth);
 
 	glClearColor(0, .2, 0, 1);
 
@@ -110,16 +133,13 @@ void Render()
 		break;
 	}
 
-	glDrawArrays(GL_TRIANGLE_FAN, 0, 50);
+	glDrawArrays(GL_TRIANGLES, 0, 3 * std::pow(3, SierpinskiDepth));
 }
 
 float theta = 0;
-void Update()
-{
-	// add all tick code
+void Update() {
 	theta += 0.0001f;
-
-	GLuint Theta_Location = glGetUniformLocation(BasiceprogramId, "theta");
+	GLuint Theta_Location = glGetUniformLocation(BasicProgramId, "theta");
 	glUniform1f(Theta_Location, theta);
 }
 
